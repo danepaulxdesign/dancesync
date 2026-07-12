e14c5819ae866f3e5861232f100701c07d575078
Dane Paul <155028231+danepaulxdesign@users.noreply.github.com>
2026-07-12 10:23:46 -0700

    Update worker.js

--- js/worker.js
+++ js/worker.js
@@ -1,1 +1,21 @@
+/**
+ * Analysis Web Worker: runs the alignment math off the UI thread.
+ * Receives { videoPcm, studioPcm, sr, videoDuration } (transferred buffers),
+ * posts { type: "progress", message } updates and a final
+ * { type: "done", analysis } or { type: "error", message }.
+ */
 
+import { analyze } from "./analyze.js";
+
+self.onmessage = (e) => {
+  const { videoPcm, studioPcm, sr, videoDuration } = e.data;
+  try {
+    const analysis = analyze(
+      videoPcm, studioPcm, sr, videoDuration,
+      (message) => self.postMessage({ type: "progress", message }),
+    );
+    self.postMessage({ type: "done", analysis });
+  } catch (err) {
+    self.postMessage({ type: "error", message: String(err?.stack || err) });
+  }
+};
